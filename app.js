604f9c6ba32090e2e7b87a8530c53bc1ca01935d
Cherish Collins <cherish.ebojudah@gmail.com>
2021-10-18 16:28:42 +0100

    created book class in app.js file

---
+++ app.js
@@ -0,0 +1,18 @@
+//Book class: Represent a book
+class Book {
+    constructor(title, author, publisher) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+    }
+}
+
+//UI class: Handle UI tasks
+
+//Store class; Handles storage
+
+//Event: Display book
+
+// Event: Add a book
+
+//Event: Remove a book

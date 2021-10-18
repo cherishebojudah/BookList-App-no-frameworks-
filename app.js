710650eccb713c6037afdd63dc11f4b634eb68f1
Cherish Collins <cherish.ebojudah@gmail.com>
2021-10-18 16:52:13 +0100

    created UI class to handle UI events

--- app.js
+++ app.js
@@ -8,6 +8,48 @@ class Book {
 }
 
 //UI class: Handle UI tasks
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'john Doe',
+                publisher: 'Penguin Books'
+            },
+
+            {
+                title: 'Book One',
+                author: 'john Doe',
+                publisher: 'Penguin Books'
+            },
+
+            {
+                title: 'Book One',
+                author: 'john Doe',
+                publisher: 'Penguin Books'
+            }
+        ];
+
+        const books = StoredBooks; 
+
+        books.forEach((book) => UI.addBookToList());
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.publisher}</td>
+            <td><a href='#' class='btn btn-danger btn-sm delete'>X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+}
 
 //Store class; Handles storage
 

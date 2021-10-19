f043b1a1ec09f38e0606e7996ae923bb0810720c
Cherish Collins <cherish.ebojudah@gmail.com>
2021-10-19 10:10:04 +0100

    add settimeout method for alert message to vanish

--- app.js
+++ app.js
@@ -63,6 +63,9 @@ class UI {
         const container = document.querySelector('.container');
         const form = document.querySelector('#book-form');
         container.insertBefore(div, form);
+
+        //Vanish in 3 secs
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
     }
 
     static clearFields() {

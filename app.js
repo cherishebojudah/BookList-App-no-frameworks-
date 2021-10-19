03c5d553abdc99ed5a684250b25ddc2439736151
Cherish Collins <cherish.ebojudah@gmail.com>
2021-10-19 11:45:23 +0100

    added add, get and remove functions from local storage

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
 //Book class: Represent a book
 class Book {
-    constructor(title, author, publisher) {
+    constructor(title, author, isbn) {
         this.title = title;
         this.author = author;
-        this.publisher = publisher;
+        this.isbn = isbn;
     }
 }
 
 //UI class: Handle UI tasks
 class UI {
     static displayBooks() {
-        const StoredBooks = [
-            {
-                title: 'A time to Kill',
-                author: 'John Grisham',
-                publisher: 'Penguin Books'
-            },
 
-            {
-                title: 'Midnight Stalker',
-                author: 'Julia Roberts',
-                publisher: 'Hallmark Books'
-            },
-
-            {
-                title: 'The Pelican Brief',
-                author: 'Dan Brown',
-                publisher: 'Whitewater Books'
-            }
-        ];
-
-        const books = StoredBooks; 
+        const books = Store.getBooks(); 
 
         books.forEach((book) => UI.addBookToList(book));
     }
@@ -43,7 +24,7 @@ class UI {
         row.innerHTML = `
             <td>${book.title}</td>
             <td>${book.author}</td>
-            <td>${book.publisher}</td>
+            <td>${book.isbn}</td>
             <td><a href='#' class='btn btn-danger btn-sm delete'>X</a></td>
         `;
 
@@ -64,14 +45,14 @@ class UI {
         const form = document.querySelector('#book-form');
         container.insertBefore(div, form);
 
-        //Vanish in 3 secs
-        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+        //Vanish in 2.5 secs
+        setTimeout(() => document.querySelector('.alert').remove(), 2500);
     }
 
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
-        document.querySelector('#publisher').value = '';
+        document.querySelector('#isbn').value = '';
     }
 }
 
@@ -88,17 +69,17 @@ class Store {
         return books;
     }
     
-    static addBooks(book) {
+    static addBook(book) {
         const books = Store.getBooks();
         books.push(book);
         localStorage.setItem('books', JSON.stringify(books));
     }
     
-    static removeBook(book) {
+    static removeBook(isbn) {
         const books = Store.getBooks();
 
         books.forEach((book, index) => {
-            if(books.title === title) {
+            if(book.isbn === isbn) {
                 books.splice(index, 1)
             }
         });
@@ -118,17 +99,20 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     //Get form values
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
-    const publisher = document.querySelector('#publisher').value;
+    const isbn = document.querySelector('#isbn').value;
 
     //Validate fields
-    if( title === '' || author === '' || publisher === '') {
+    if( title === '' || author === '' || isbn === '') {
         UI.showAlert('Please fill in all fields', 'danger');
     } else {
         //Instantiate a new book
-        const book = new Book(title, author, publisher);
+        const book = new Book(title, author, isbn);
 
         //Add book to list
         UI.addBookToList(book);
+
+        //Add book to storage
+        Store.addBook(book);
 
         //Show success message
         UI.showAlert('Book Added', 'success')
@@ -140,7 +124,11 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
 //Event: Remove a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
+    //Remove book from UI
     UI.deleteBook(e.target);
+
+    //Remove book from storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     //Show success message
     UI.showAlert('Book Removed', 'info');

ef3ebe63e45a196e513b76e8d06f00840f9685f3
Cherish Collins <cherish.ebojudah@gmail.com>
2021-10-19 10:53:08 +0100

    created local storage

--- app.js
+++ app.js
@@ -78,15 +78,32 @@ class UI {
 //Store class; Handles storage
 class Store {
     static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
         
+        return books;
     }
     
     static addBooks(book) {
-
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
     }
     
     static removeBook(book) {
+        const books = Store.getBooks();
 
+        books.forEach((book, index) => {
+            if(books.title === title) {
+                books.splice(index, 1)
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
     }
 }
 
